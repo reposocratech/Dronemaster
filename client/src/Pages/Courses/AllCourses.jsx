773e8315a6b7a59a0095d4cef3c401cc51368335
“MiguelMachuca” <miguemachu@gmail.com>
2023-08-21 08:00:46 +0200

    Teacher Landing, course Info

--- client/src/Pages/Courses/AllCourses.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const AllCourses = () => {
-    return (
-        <div>CoursesApp</div>
-    )
-}
--- client/src/context/DroneMasterProvider.jsx
+++ client/src/context/DroneMasterProvider.jsx
@@ -1,74 +1,48 @@
-import React, { createContext, useEffect, useState } from 'react'
-import { getLocalStorage } from '../helper/localStorageDroneMaster'
-import jwDecode from 'jwt-decode'
+import React, { createContext, useEffect, useState } from "react";
+import { getLocalStorage } from "../helper/localStorageDroneMaster";
+import jwDecode from "jwt-decode";
 
-import axios from 'axios'
+import axios from "axios";
 
+export const DroneMasterContext = createContext();
 
-export const DroneMasterContext = createContext()
-
-const DroneMasterProvider = ({children}) => {
-
-
+const DroneMasterProvider = ({ children }) => {
   const [token, setToken] = useState();
   const [user, setUser] = useState();
   const [isLogged, setIsLogged] = useState();
 
-  useEffect(() =>{
-
-
+  useEffect(() => {
     const tokenLocalStorage = getLocalStorage("token");
 
-    
-    setToken(tokenLocalStorage)
-   
-   if(tokenLocalStorage){
-       const id = jwDecode(tokenLocalStorage).user.id;
-   
-       axios
-       .get()
-       .then((res)=>{
-         console.log("La respuesta del context*********",res)
-               setUser(res.data.resultUser[0])
-               setIsLogged(true);
-               
-       })
-       .catch()
-       
-   }
-   
-   }, [])
-   
-   console.log("*************************", user)
+    setToken(tokenLocalStorage);
 
+    if (tokenLocalStorage) {
+      const id = jwDecode(tokenLocalStorage).user.id;
 
+      axios
+        .get()
+        .then((res) => {
+          setUser(res.data.resultUser[0]);
+          setIsLogged(true);
+        })
+        .catch();
+    }
+  }, []);
 
   return (
-
-
-
-
     <div>
-
-      <DroneMasterContext.Provider value ={{
-
-          user, 
+      <DroneMasterContext.Provider
+        value={{
+          user,
           setUser,
           token,
-          setToken
-
-
-      }}>
-
-         {children}
-
+          setToken,
+        }}
+      >
+        {children}
       </DroneMasterContext.Provider>
-    
-
-       
-      
     </div>
-  )
-}
+  );
+};
 
-export default DroneMasterProvider
+export default DroneMasterProvider;
