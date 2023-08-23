465456cc02c1932e8eeeb381480a6b9e3d5f45d6
Paula <palacioscampanopaula@gmail.com>
2023-08-23 13:10:31 +0200

    correccion errorer

--- client/src/Pages/Home/HomeApp.jsx
+++ client/src/Pages/Home/HomeApp.jsx
@@ -1,4 +1,3 @@
-
 import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import "../AllCourses/allCoursesStyle.scss";
@@ -24,8 +23,7 @@ const HomeApp = () => {
   const [counterRatio, setCounterRatio] = useState(1);
   const [counter1, setCounter1] = useState(0);
   const [counterRatio1, setCounterRatio1] = useState(1);
-  const navigate = useNavigate()
-
+  const navigate = useNavigate();
 
   const currentWidth = () => {
     const widthScreen = document.body.clientWidth;
@@ -95,21 +93,74 @@ const HomeApp = () => {
 
   return (
     <Container fluid className="home">
-      {width > 991 && <Row>
-        <Col className="homeInfo">
-          <div>
-            <h1>
-              COMIENZA EN EL MUNDO DE LOS <span>DRONES</span> CON NUESTROS
-              CURSOS
-            </h1>
-            <p>Especializate en el sector mas demandado de Europa</p>
-            <div className="py-4">
-              <button className="btnNormal" onClick={() => { navigate('/login') }}>
-                Comenzar
-              </button>
+      {width > 991 && (
+        <Row>
+          <Col className="homeInfo">
+            <div>
+              <h1>
+                COMIENZA EN EL MUNDO DE LOS <span>DRONES</span> CON NUESTROS
+                CURSOS
+              </h1>
+              <p>Especializate en el sector mas demandado de Europa</p>
+              <div className="py-4">
+                <button
+                  className="btnNormal"
+                  onClick={() => {
+                    navigate("/login");
+                  }}
+                >
+                  Comenzar
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="allCounter pe-4 py-5">
+            <div className="allCounter pe-4 py-5">
+              <div>
+                <h2 className="homeCounter">{counterInfo.TotalTeachers}</h2>
+                <p className="userCounter">Docentes</p>
+              </div>
+              <div>
+                <h2 className="homeCounter">{counterInfo.TotalCourse}</h2>
+                <p className="userCounter">Cursos</p>
+              </div>
+              <div>
+                <h2 className="homeCounter">{counterInfo.TotalStudents}</h2>
+                <p className="userCounter">Alumnos</p>
+              </div>
+            </div>
+          </Col>
+          <Col className="homeImage">
+            <div className="orangeCircle d-flex justify-content-center align-items-center">
+              <img className="drone_img" src={drone_home} alt="" />
+            </div>
+          </Col>
+        </Row>
+      )}
+      {width < 992 && (
+        <Row>
+          <Col className="homeInfoMobile">
+            <div className="infoLabel d-flex justify-content-between">
+              <div className=" w-md-50 px-3">
+                <h1 className="text-center">
+                  COMIENZA EN EL MUNDO DE LOS <span>DRONES</span> CON NUESTROS
+                  CURSOS
+                </h1>
+                <p className="text-center">
+                  Especializate en el sector mas demandado de Europa
+                </p>
+                <div className="pt-4 text-center">
+                  <button
+                    className="btnNormal bg-dark"
+                    onClick={() => {
+                      navigate("/login");
+                    }}
+                  >
+                    Comenzar
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <div className="allCounter d-flex justify-content-center px-5 pt-0 pt-md-4 pb-5 pb-lg-5">
             <div>
               <h2 className="homeCounter">{counterInfo.TotalTeachers}</h2>
               <p className="userCounter">Docentes</p>
@@ -123,52 +174,10 @@ const HomeApp = () => {
               <p className="userCounter">Alumnos</p>
             </div>
           </div>
-        </Col>
-        <Col className='homeImage'>
-          <div className='orangeCircle d-flex justify-content-center align-items-center'>
-            <img className='drone_img' src={drone_home} alt="" />
-          </div>
-        </Col>
-      </Row>}
-      {width < 992 && <Row>
-        <Col className="homeInfoMobile">
-          <div className='infoLabel d-flex justify-content-between'>
-            <div className=' w-md-50 px-3'>
-              <h1 className='text-center'>
-                COMIENZA EN EL MUNDO DE LOS <span>DRONES</span> CON NUESTROS
-                CURSOS
-              </h1>
-              <p className='text-center'>Especializate en el sector mas demandado de Europa</p>
-              <div className="pt-4 text-center">
-                <button
-                  className="btnNormal bg-dark"
-                  onClick={() => {
-                    navigate("/login");
-                  }}
-                >
-                  Comenzar
-                </button>
-              </div>
-            </div>
-          </div>
-        </Col>
-        <div className="allCounter d-flex justify-content-center px-5 pt-0 pt-md-4 pb-5 pb-lg-5">
-          <div>
-            <h2 className="homeCounter">{counterInfo.TotalTeachers}</h2>
-            <p className="userCounter">Docentes</p>
-          </div>
-          <div>
-            <h2 className="homeCounter">{counterInfo.TotalCourse}</h2>
-            <p className="userCounter">Cursos</p>
-          </div>
-          <div>
-            <h2 className="homeCounter">{counterInfo.TotalStudents}</h2>
-            <p className="userCounter">Alumnos</p>
-          </div>
-        </div>
-      </Row>}
-      <div className='homeMessages d-flex-column justify-content-center justify-content-lg-start text-center text-lg-start'>
-        <div className='homeOneMessage'>
+        </Row>
+      )}
+      <div className="homeMessages d-flex-column justify-content-center justify-content-lg-start text-center text-lg-start">
+        <div className="homeOneMessage">
           <h3>Aprende</h3>
           <p>Domina el arte del manejo de drones en diversos sectores</p>
         </div>
@@ -178,89 +187,93 @@ const HomeApp = () => {
             <p>Descubre nuevas posibilidades con la tecnología de drones</p>
           </div>
         </div>
-        <div className='d-flex justify-content-center justify-content-lg-end text-center text-lg-start'>
-          <div className='homeOneMessage pb-0'>
+        <div className="d-flex justify-content-center justify-content-lg-end text-center text-lg-start">
+          <div className="homeOneMessage pb-0">
             <h3>Eleva tus habilidades</h3>
             <p>Impulsa tu carrera con formación especializada en drones</p>
           </div>
         </div>
       </div>
-      <div className='allCoursesContainer p-0 px-sm-5'>
+      <div className="allCoursesContainer p-0 px-sm-5">
         <div className="categoryContainer">
-          <h2 className="categoryTitle text-center text-md-start">Últimos cursos</h2>
+          <h2 className="categoryTitle text-center text-md-start">
+            Últimos cursos
+          </h2>
           <div className="courseCardContainerWrapper">
-            {counter !== 0 ?
+            {counter !== 0 ? (
               <div className="navigationButtonContainerLeft">
                 <MdNavigateBefore
                   className="navigationButton"
                   onClick={() => setCounter(counter - counterRatio)}
                 />
-              </div> : <div className="navigationButtonContainerLeft">
-                <MdNavigateBefore
-                  className="navigationButton opacity-0"
-                />
               </div>
-            }
+            ) : (
+              <div className="navigationButtonContainerLeft">
+                <MdNavigateBefore className="navigationButton opacity-0" />
+              </div>
+            )}
             <div className="courseCardContainer">
-              {topCourses?.slice(counter, counter + counterRatio).map((elem) => {
-                return (
-                  <CourseCard key={elem.course_id} oneCourse={elem} />
-                )
-              })}
+              {topCourses
+                ?.slice(counter, counter + counterRatio)
+                .map((elem) => {
+                  return <CourseCard key={elem.course_id} oneCourse={elem} />;
+                })}
             </div>
-            {counter + counterRatio < topCourses?.length ?
+            {counter + counterRatio < topCourses?.length ? (
               <div className="navigationButtonContainerRight">
                 <MdNavigateNext
                   className="navigationButton"
                   onClick={() => setCounter(counter + counterRatio)}
                 />
-              </div> : <div className="navigationButtonContainerRight">
-                <MdNavigateNext
-                  className="navigationButton opacity-0"
-                />
               </div>
-            }
+            ) : (
+              <div className="navigationButtonContainerRight">
+                <MdNavigateNext className="navigationButton opacity-0" />
+              </div>
+            )}
           </div>
         </div>
         <div className="categoryContainer pt-4">
-          <h2 className="categoryTitle text-center text-md-start">Cursos mejor valorados</h2>
+          <h2 className="categoryTitle text-center text-md-start">
+            Cursos mejor valorados
+          </h2>
           <div className="courseCardContainerWrapper">
-            {counter1 !== 0 ?
+            {counter1 !== 0 ? (
               <div className="navigationButtonContainerLeft">
                 <MdNavigateBefore
                   className="navigationButton"
                   onClick={() => setCounter1(counter1 - counterRatio1)}
                 />
-              </div> : <div className="navigationButtonContainerLeft">
-                <MdNavigateBefore
-                  className="navigationButton opacity-0"
-                />
               </div>
-            }
+            ) : (
+              <div className="navigationButtonContainerLeft">
+                <MdNavigateBefore className="navigationButton opacity-0" />
+              </div>
+            )}
             <div className="courseCardContainer">
-              {bestRatedCourses?.slice(counter1, counter1 + counterRatio1).map((elem) => {
-                return (
-                  <CourseCard key={elem.course_id} oneCourse={elem} />
-                )
-              })}
+              {bestRatedCourses
+                ?.slice(counter1, counter1 + counterRatio1)
+                .map((elem) => {
+                  return <CourseCard key={elem.course_id} oneCourse={elem} />;
+                })}
             </div>
-            {counter1 + counterRatio1 < bestRatedCourses?.length ?
+            {counter1 + counterRatio1 < bestRatedCourses?.length ? (
               <div className="navigationButtonContainerRight">
                 <MdNavigateNext
                   className="navigationButton"
                   onClick={() => setCounter1(counter1 + counterRatio1)}
                 />
-              </div> : <div className="navigationButtonContainerRight">
-                <MdNavigateNext
-                  className="navigationButton opacity-0"
-                />
               </div>
-            }
+            ) : (
+              <div className="navigationButtonContainerRight">
+                <MdNavigateNext className="navigationButton opacity-0" />
+              </div>
+            )}
           </div>
         </div>
       </div>
-    </Container >
+    </Container>
   );
+};
 
 export default HomeApp;
-
